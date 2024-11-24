8b6fd5f64f5a76b781cdeb3f06eadddfd29df7d8
MatiXdou <163367319+MatiXdou@users.noreply.github.com>
2024-11-24 00:17:58 -0300

    cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideFirebaseApp(() => initializeApp({"projectId":"tareapp-c468e","appId":"1:227327226891:web:61eed8ab87e6d1e886030e","storageBucket":"tareapp-c468e.firebasestorage.app","apiKey":"AIzaSyDHnDfa4-xAOdQ0sTYaTmFtbQznAR9hjUY","authDomain":"tareapp-c468e.firebaseapp.com","messagingSenderId":"227327226891","measurementId":"G-1CX2ZEN914"})), provideAuth(() => getAuth()), provideAnalytics(() => getAnalytics()), ScreenTrackingService, UserTrackingService, provideFirestore(() => getFirestore())],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideFirebaseApp(() => initializeApp({})), provideAuth(() => getAuth()), provideAnalytics(() => getAnalytics()), ScreenTrackingService, UserTrackingService, provideFirestore(() => getFirestore())],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
